4974f1a6390e43ab6be3320926a3292ef883a346
felenitaribeiro <felenita.ribeiro@gmail.com>
2023-05-01 19:51:33 +1000

    eye tracking data download script

--- data/eyetracking/eyeTrackingData_download.sh
+++ data/eyetracking/eyeTrackingData_download.sh
@@ -7,4 +7,19 @@ do
     aws s3 cp \
         s3://hcp-openaccess/HCP_1200/$subject/unprocessed/7T/tfMRI_RETBAR1_AP/LINKED_DATA/EYETRACKER/"$subject"_7T_RETBAR1_eyetrack.asc \
         .
+    aws s3 cp \
+        s3://hcp-openaccess/HCP_1200/$subject/unprocessed/7T/tfMRI_RETBAR2_PA/LINKED_DATA/EYETRACKER/"$subject"_7T_RETBAR2_eyetrack.asc \
+        .
+    aws s3 cp \
+        s3://hcp-openaccess/HCP_1200/$subject/unprocessed/7T/tfMRI_RETCCW_AP/LINKED_DATA/EYETRACKER/"$subject"_7T_RETCCW_eyetrack.asc \
+        .
+    aws s3 cp \
+        s3://hcp-openaccess/HCP_1200/$subject/unprocessed/7T/tfMRI_RETCON_PA/LINKED_DATA/EYETRACKER/"$subject"_7T_RETCON_eyetrack.asc \
+        .
+    aws s3 cp \
+        s3://hcp-openaccess/HCP_1200/$subject/unprocessed/7T/tfMRI_RETCW_PA/LINKED_DATA/EYETRACKER/"$subject"_7T_RETCW_eyetrack.asc \
+        .
+    aws s3 cp \
+        s3://hcp-openaccess/HCP_1200/$subject/unprocessed/7T/tfMRI_RETEXP_AP/LINKED_DATA/EYETRACKER/"$subject"_7T_RETEXP_eyetrack.asc \
+        .
 done < $subjectlist
